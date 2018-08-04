4240c4c91ea6325ea4d550b98c563465b0dffc79
Aleksey Bykhun <theisease@gmail.com>
2018-08-05 02:04:45 +0300

    use /api

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ const config = {
     root: path.join(__dirname, '..'),
     port: process.env.PORT || 9000,
     ip: process.env.IP || '0.0.0.0',
-    apiRoot: process.env.API_ROOT || '',
+    apiRoot: process.env.API_ROOT || '/api',
     masterKey: requireProcessEnv('MASTER_KEY'),
     jwtSecret: requireProcessEnv('JWT_SECRET'),
     mongo: {
